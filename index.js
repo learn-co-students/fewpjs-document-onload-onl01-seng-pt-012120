9d57334bdab1e8dccd4d48668e6d0f1375260bc9
javers <joeychavira2016@gmail.com>
2020-08-05 21:35:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+  const str = document.getElementById('text');
+  str.innerHTML = "This is really cool!"
+} );
+
+
